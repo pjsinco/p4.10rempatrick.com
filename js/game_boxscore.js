d1060725bc3fb5833c5c39fb0770a02db9276790
Patrick Sinco <psinco@gmail.com>
2013-12-25 18:47:37 -0600

    wires up new-game button

--- js/game_boxscore.js
+++ js/game_boxscore.js
@@ -1,3 +1,29 @@
 (function() {
   $('table.boxscore tr:even').css('background-color', '#f5f5f5');
+
+  $('#new-game').click(function() {
+    $('<div></div>').appendTo('body')
+      .html('<div><h6>Are you sure you want '
+          + 'to leave the box score and start a new game?')
+      .dialog({
+        modal: true,
+        title: 'New game?',
+        zIndex: 10000,
+        autoOpen: true,
+        width: 'auto',
+        resizable: false,
+        buttons: {
+          Yes: function() {
+            document.location.href = '/';
+          },
+          No: function() {
+            $(this).dialog('close');
+          }
+        },
+        close: function(event, ui) {
+          $(this).remove();
+        }
+      });
+    });
+
 })();
